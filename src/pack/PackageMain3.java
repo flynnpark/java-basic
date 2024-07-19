f73facd338a175e3bfeb51cac0230cc55ff6f0d3
Flynn <wphestiraid@gmail.com>
2024-07-19 09:08:32 +0900

    패키지: import

---
+++ src/pack/PackageMain3.java
@@ -0,0 +1,11 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain3 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        User user1 = new User();
+        pack.b.User user2 = new pack.b.User();
+    }
+}
